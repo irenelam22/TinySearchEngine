eae8e6c4feaf90465fa2b8680660fcbda4083e84
Irene X. Lam 22 <ilam@moose.cs.dartmouth.edu>
2020-07-27 20:53:41 -0400

    Made successfully

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -11,12 +11,17 @@
 #include <string.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <unistd.h>
 #include "../libcs50/bag.h"
 #include "../libcs50/hashtable.h"
 #include "../libcs50/webpage.h"
+#include "../libcs50/memory.h"
 
 bool inputCheck(webpage_t *seedURL, char* pageDir, int maxDepth);
-void crawl(char* seedURL, char* pageDir, int maxDepth);
+void crawler(webpage_t* seedURL, char* pageDir, int maxDepth);
+char* pagefetcher(webpage_t *page);
+bool pagesaver(webpage_t *page, char* pageDir, int id);
+void pagescanner(webpage_t *page, hashtable_t *visited, int depth);
 
 int main(int argc, char* argv[])
 {
@@ -27,16 +32,20 @@ int main(int argc, char* argv[])
         fprintf(stderr, "Too many arguments provided\n");
     }
     // Normalize seedURL
-    webpage_t *seedURL = webpage_new(argv[1], 0, NULL);
-    if (!NormalizeURL) {
-        fprintf(stderr, "Error normalizing seedURL\n");
+    char* seedURL = argv[1];
+    if (!NormalizeURL(seedURL)) {
+        fprintf(stderr, "Error normalizing seedURL %s\n", seedURL);
         return 1;
     }
-    if (!inputCheck(seedURL, argv[2], argv[3])) {
-        return 1;
+    // Making a copy on the heap
+    char* urlcopy = assertp(malloc(strlen(seedURL)+1), "url copy");
+    strcpy(urlcopy, seedURL);
+	webpage_t *web = webpage_new(urlcopy, 0, NULL); 
+    if (!inputCheck(web, argv[2], atoi(argv[3]))) {
+        return 2;
     }
-    crawler(argv[1], argv[2], argv[3]);
-    
+    crawler(web, argv[2], atoi(argv[3]));
+    webpage_delete(web);
     return 0;
 }
 
@@ -46,7 +55,7 @@ bool inputCheck(webpage_t *seedURL, char* pageDir, int maxDepth)
         fprintf(stderr, "seedURL is NULL\n");
         return false;
     }
-    if (!IsInternalURL) {
+    if (!IsInternalURL(webpage_getURL(seedURL))) {
         fprintf(stderr, "seedURL is not an internal URL\n");
         return false;
     }
@@ -59,9 +68,10 @@ bool inputCheck(webpage_t *seedURL, char* pageDir, int maxDepth)
         fprintf(stderr, "maxDepth must be within the range [0,10]\n");
         return false;
     }
+    return true;
 }
 
-void crawler(char* seedURL, char* pageDir, int maxDepth)
+void crawler(webpage_t* seedURL, char* pageDir, int maxDepth)
 {
     if (maxDepth < 0) {
         return;
@@ -70,19 +80,79 @@ void crawler(char* seedURL, char* pageDir, int maxDepth)
     bag_t *bag = bag_new();
     hashtable_t *visited = hashtable_new(10);
     int currDepth = 0;
+    int id = 0;
 
     bag_insert(bag, seedURL);
-    hashtable_insert(visited, seedURL, currDepth);
-
-    while ((webpage_t *item = bag_extract(bag)) != NULL) {
+    hashtable_insert(visited, webpage_getURL(seedURL), &currDepth);
+	
+    webpage_t* item;
+    while ((item = bag_extract(bag)) != NULL) {
+        sleep(60);
+        pagefetcher(item);
+        if (pagesaver(item, pageDir, id)) {
+            id++;
+        }
+        else {
+            break;
+        }
         
+        if (currDepth < maxDepth) {
+            pagescanner(item, visited, currDepth);
+            currDepth++;
+        }
+    }
+}
+
+
+char* pagefetcher(webpage_t *page)
+{
+    if (webpage_fetch(page)) { 
+		return webpage_getHTML(page);
+	}
+	else { // failed to fetch the page
+		fprintf(stderr, "failed to fetch %s\n", webpage_getHTML(page));
+		webpage_delete(page);
+		return NULL;
+	}
+}
+		
+void pagescanner(webpage_t *page, hashtable_t *visited, int depth)
+{
+    if (page == NULL) {
+        fprintf(stderr, "pagescanner received invalid webpage\n");
+    }
+    char* next;
+    while ((next = webpage_getNextURL(page, 0)) != NULL) {
+        // Normalize next URL
+        if (!NormalizeURL(next)) {
+            fprintf(stderr, "Error normalizing next URL %s\n", next);
+            return;
+        }
+        if (!IsInternalURL(next)) {
+            continue;
+        }
+        if (hashtable_insert(visited, next, &depth)) {
+            printf("TODO: e12");
+            // 12. make a new *webpage* for that URL, at depth+1
+	        // 13. add the new webpage to the *bag* of webpages to be crawled
+        }
     }
 
-    
+
 }
 
 // outputs a page to the the appropriate file
-void pagesaver(webpage_t *seedURL, char* pageDir) 
+bool pagesaver(webpage_t *page, char* pageDir, int id) 
 {
+    // TODO: pageDir/id
+    char* num = malloc(40);
+    sprintf(num, "%d", id);
+    FILE *fp = fopen(strcat(pageDir, num), "w"); 
+	assertp(fp, "cannot open file for writing\n");
 
+	fprintf(fp, "%s\n%d\n%s", webpage_getURL(page), id, webpage_getHTML(page));
+
+	fclose(fp);
+    free(num);
+    return true;
 }
